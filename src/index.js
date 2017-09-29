f7393c9cc62caf15a8b29e3c391000b171090ba8
unknown <philipok4596@gmail.com>
2017-09-29 23:56:38 +0300

    zeros/feature: implement zeros function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function zeros(expression) {
-  // your solution
+  var arr = expression.split("*");
+  var twoCount = 0;
+  var fiveCount = 0;
+  var arr = arr.forEach(function(elem){
+    var num = parseInt(elem);
+    while (num > 0)
+    {
+      var tempNum = num;
+      while (true){
+        if (tempNum % 10 == 0){tempNum /= 10; twoCount++; fiveCount++;}
+        else if (tempNum % 5 == 0) {tempNum /= 5; fiveCount++;}
+        else if (tempNum % 2 == 0) {tempNum /= 2; twoCount++;}
+        else break;
+      }
+      if (elem.slice(elem.length-2, elem.length) === '!!') num -= 2;
+      else num--;
+    }
+  });
+  return Math.min(twoCount, fiveCount);
 }
